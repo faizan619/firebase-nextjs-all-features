df762d24587a8d1b378e540916817f965bec2b7d
faizan619 <alamf6023@gmail.com>
2024-01-14 20:53:24 +0530

    minor change

--- src/app/realtimedb/view/page.js
+++ src/app/realtimedb/view/page.js
@@ -16,12 +16,15 @@ export default async function Page() {
     return (
       <div className="bg-black min-h-screen text-white gap-5 flex flex-col items-center">
         <h1>View Details here</h1>
+              <Button page={`/realtimedb`} title="Back"/>
         {details &&
           Object.keys(details).map((key) => (
-            <div key={key} className="border p-3 rounded-md">
+            <div key={key} className="border p-3 rounded-md w-[80%]">
               <p>Unique Id: {key}</p>
               <p>Name: {details[key].name}</p>
+              <div className="flex justify-between">
               <Button page={`view/${key}`} title="View Details" />
+              </div>
               {/* <p>Email: {details[key].email}</p>
                             <p>Phone: {details[key].phone}</p>
                             <p>Message: {details[key].message}</p> */}
