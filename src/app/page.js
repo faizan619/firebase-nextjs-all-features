ca743f950a6e04b5df696767f7c3d6043afd505d
faizan619 <alamf6023@gmail.com>
2024-01-16 22:57:07 +0530

    added new route

--- src/app/page.js
+++ src/app/page.js
@@ -5,11 +5,12 @@ import { useRouter } from 'next/navigation'
 export default function Home() {
   const router = useRouter();
   return (
-    <div className='flex flex-col bg-[#060606] min-h-screen text-white items-center'>
+    <div className='flex flex-col bg-[#060606] min-h-screen text-white items-center'> 
       <h1>Welcome to Firebase Practise Site.</h1>
       <div className='py-10 px-5 gap-5 border w-[80%] flex flex-col items-center rounded-md'>
         <button className='border px-5 py-3 rounded-md hover:bg-gray-100 hover:text-black transition-all' onClick={()=>router.push("/realtimedb")}>Real time Database</button>
         <button className='border px-5 py-3 rounded-md hover:bg-gray-100 hover:text-black transition-all' onClick={()=>router.push("/storage")}>Storage Database</button>
+        <button className='border px-5 py-3 rounded-md hover:bg-gray-100 hover:text-black transition-all' onClick={()=>router.push("/storefire")}>Storage and Firestore</button>
       </div>
     </div>
   )
